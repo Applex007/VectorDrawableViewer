6c0e8dabc05586ade83dc1359d502dbe32fb0aff
Applex007 <13167178850@163.com>
2016-11-16 15:39:34 +0800

    resovle bug: show directory when open file

--- src/com/applex/main/VectorDrawableFileOpen.java
+++ src/com/applex/main/VectorDrawableFileOpen.java
@@ -6,29 +6,31 @@ import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileFilter;
 
 public class VectorDrawableFileOpen extends JFileChooser{
-	
+
 	public VectorDrawableFileOpen() {
 		super();
 		setDialogTitle(DefaultValues.OPEN_FILE);
 		setFileSelectionMode(FILES_ONLY);
 		setAcceptAllFileFilterUsed(false);
 		setFileFilter(new FileFilter() {
-			
+
 			@Override
 			public String getDescription() {
 				// TODO Auto-generated method stub
 				return "Android VectorDrawable File(*.xml)";
 			}
-			
+
 			@Override
 			public boolean accept(File f) {
 				// TODO Auto-generated method stub
-				if (f.getName().toLowerCase().endsWith(".xml")) {
+				if (f.isDirectory()) {
+				return true;
+			}else	if (f.getName().toLowerCase().endsWith(".xml")) {
 					return true;
 				}
 				return false;
 			}
 		});
 	}
-	
+
 }
